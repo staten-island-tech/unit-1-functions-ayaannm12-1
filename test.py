805f587df637d6156fc16a9225854359f98b772a
ayaannm12 <ayaanm5352@gmail.com>
2025-09-15 10:08:51 -0400

    ytfugjlh

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print("hello")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape = 'turtle'
+
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+# square(100,90)
+
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+doubleSquares(5)
+
+turtle.done()
